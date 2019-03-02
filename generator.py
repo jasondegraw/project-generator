bb256223c7768a8ee4a7a943cf60e7eb5dca8ce3
Jason W. DeGraw <jason.degraw@gmail.com>
2019-03-02 15:28:14 -0700

    Fix up code a bit

--- generator.py
+++ generator.py
@@ -1,3 +1,31 @@
+# Copyright (c) 2018, Jason DeGraw
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 import os
 
 toplevel_cmakelists_txt = {
@@ -22,13 +50,41 @@ if(CMAKE_COMPILER_IS_GNUCXX)
   endif()
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 elseif(MSVC)
-  # http://en.wikipedia.org/wiki/Visual_C%2B%2B#32-bit_and_64-bit_versions
+  # http://en.wikipedia.org/wiki/Visual_C%%2B%%2B#32-bit_and_64-bit_versions
   if(${CMAKE_C_COMPILER_VERSION} VERSION_LESS "18.0.21005.1")
     message(FATAL_ERROR "Visual Studio earlier than VS2013 is not supported")
   endif()
 elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   # Need to check for clang version
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++")
+endif()
+
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+
+add_subdirectory(src)
+''',
+    'c++17' : '''
+cmake_minimum_required(VERSION 2.8.11)
+
+project(%s)
+
+if(CMAKE_COMPILER_IS_GNUCXX)
+  # 4.6 is the earliest version supporting nullptr and range-based for
+  # https://gcc.gnu.org/projects/cxx-status.html
+  if(${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "7.0.0")
+    message(FATAL_ERROR "g++ versions earlier than 7.0.0 are not supported")
+  endif()
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
+elseif(MSVC)
+  # http://en.wikipedia.org/wiki/Visual_C%%2B%%2B#32-bit_and_64-bit_versions
+  if(${CMAKE_C_COMPILER_VERSION} VERSION_LESS "19.10.25017.0")
+    message(FATAL_ERROR "Visual Studio earlier than VS2017 is not supported")
+  endif()
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++latest")
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+  # Need to check for clang version
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -stdlib=libc++")
 endif()
 
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
@@ -49,9 +105,12 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='.')
     #parser.add_argument('project', metavar='PROJECT')
-    parser.add_argument('-o', '--output', dest='outfile', action='store',
-                        default='std211.xml',
-                        help='file to save BSXML output in')
+    #parser.add_argument('-o', '--output', dest='outpath', action='store',
+    #                    default='.',
+    #                    help='path to generate output in')
+    parser.add_argument('-s', '--standard', dest='standard', action='store',
+                        default='vanilla',
+                        help='C++ standard to use')
     parser.add_argument('-p', '--project', dest='project', action='store',
                         default='project',
                         help='specify project name')
@@ -60,7 +119,10 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    txt = toplevel_cmakelists_txt['vanilla'] % args.project
+    if not args.standard in toplevel_cmakelists_txt:
+        args.standard = 'vanilla'
+
+    txt = toplevel_cmakelists_txt[args.standard] % args.project
 
     fp = open('CMakeLists.txt', 'w')
     fp.write(txt)
